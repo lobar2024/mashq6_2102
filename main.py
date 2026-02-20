32d6946f1d152a281c6f188f850aa39e0721a0b2
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-02-21 00:47:52 +0500

    Add initial implementation of Shop and OnlineShop classes

---
+++ main.py
@@ -0,0 +1,20 @@
+#6
+# class Shop:
+#     def __init__(self, name, adress):
+#         self.name = name
+#         self.adress = adress
+#
+#     def open(self):
+#         print('Dokon ochildi')
+#
+# class OnlineShop(Shop):
+#     def __init__(self, name, adress, website):
+#         super().__init__(name, adress)
+#         self.website = website
+#
+#     def open(self):
+#         super().open()
+#         print(f'Saytimiz: {self.name}\nSayt manzili: {self.adress}\nWebsaytimiz: {self.website}')
+#
+# shoop = OnlineShop('Uzum market', 'Tashkent', 'uzuum.com')
+# shoop.open()
